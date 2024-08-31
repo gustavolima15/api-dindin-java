ad84dc9a87c99604f77e54782106d02bebce875f
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-08-31 09:47:31 -0300

    feat: user model

--- src/main/java/com/dindin/banco/model/Usuario.java
+++ src/main/java/com/dindin/banco/model/Usuario.java
@@ -8,16 +8,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "usuario")
+@Table(name = "usuario", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "email")
+})
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class Usuario {
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String email;
-    String senha;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 100)
+    private String senha;
 }
